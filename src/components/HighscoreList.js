42749d21859bc1533f3fb0cd0e96879bdb939d63
covertcodemn1 <45843535+covertcodemn1@users.noreply.github.com>
2020-02-22 12:17:29 +0100

    next try

--- src/components/HighscoreList.js
+++ src/components/HighscoreList.js
@@ -33,7 +33,7 @@ export class HighscoreList extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.map(user => (
+            {this.state.users.map(user => (
               <tr key={user.ID}>
                 <td>{user.UserID}</td>
                 <td>{user.UserName}</td>
