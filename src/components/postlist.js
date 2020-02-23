c1ca4bf5d641083a7be01ab2c25d780ba1adaa2f
covertcodemn1 <45843535+covertcodemn1@users.noreply.github.com>
2020-02-23 08:27:09 +0100

    postlisted

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export default class App extends Component {
+export class postlist extends Component {
   state = {
     users: [],
     isLoading: true,
