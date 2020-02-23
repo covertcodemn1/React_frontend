add8f06b11de66f4efabb786c7b4072c302a51e8
covertcodemn1 <45843535+covertcodemn1@users.noreply.github.com>
2020-02-23 13:57:30 +0100

    postlist ed

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -1,51 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 
-export class postlist extends Component {
+export default class PersonList extends React.Component {
   state = {
-    users: [],
-    isLoading: true,
-    errors: null
+    users: []
   };
+
   componentDidMount() {
     axios
-      .get("https://schnitzeljagdar.herokuapp.com/users/getAllUser")
-      .then(response =>
-        response.data.results.map(user => ({
-          username: `${user.username}`,
-          email: `${user.email}`
-        }))
-      )
-      .then(users => {
-        this.setState({
-          users,
-          isLoading: false
-        });
-      })
-      .catch(error => this.setState({ error, isLoading: false }));
+      .get(`https://schnitzeljagdar.herokuapp.com/users/getAllUser`)
+      .then(res => {
+        const users = res.data;
+        this.setState({ users });
+      });
   }
+
   render() {
-    const { isLoading, users } = this.state;
     return (
-      <React.Fragment>
-        <h2>User</h2>
-        <div>
-          {!isLoading ? (
-            users.map(user => {
-              const { username, email } = user;
-              return (
-                <div key={username}>
-                  <p>{username}</p>
-                  <p>{email}</p>
-                  <hr />
-                </div>
-              );
-            })
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-      </React.Fragment>
+      <ul>
+        {this.state.users.map(person => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
     );
   }
 }
