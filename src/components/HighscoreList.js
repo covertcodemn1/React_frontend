210a5966991fbda5de131733f9b29b4ba050dafe
covertcodemn1 <45843535+covertcodemn1@users.noreply.github.com>
2020-02-24 10:58:21 +0100

    highscore

--- src/components/HighscoreList.js
+++ src/components/HighscoreList.js
@@ -2,33 +2,21 @@ import React from "react";
 import axios from "axios";
 
 export class HighscoreList extends React.Component {
-  state = {
-    users: [],
-    isLoading: true,
-    errors: null
-  };
-
-  getUsers() {
-    axios
-      .get("https://schnitzeljagdar.herokuapp.com/users/getAllUser")
-      .then(
-        response =>
-          (this.state = response.data.results.map(user => ({
-            username: `${user.login.username}`,
-            email: `${user.email}`
-          })))
-      )
-      .then(users => {
-        this.setState({
-          users,
-          isLoading: false
-        });
-      })
-      .catch(error => this.setState({ error, isLoading: false }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
   }
-
   componentDidMount() {
-    this.getUsers();
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
@@ -36,22 +24,6 @@ export class HighscoreList extends React.Component {
     return (
       <React.Fragment>
         <h2>User</h2>
-        <div>
-          {!isLoading ? (
-            users.map(user => {
-              const { username, email } = user;
-              return (
-                <div key={email}>
-                  <p>{username}</p>
-                  <p>{email}</p>
-                  <hr />
-                </div>
-              );
-            })
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
       </React.Fragment>
     );
   }
