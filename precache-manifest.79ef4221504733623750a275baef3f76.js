deaba773ce312dbbba134cf30170f5d94d6cbfbf
covertcodemn1 <45843535+covertcodemn1@users.noreply.github.com>
2020-03-31 10:06:03 +0200

    Updates

---
+++ precache-manifest.79ef4221504733623750a275baef3f76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd274e69f06b644d7eae24276dcaa777",
+    "url": "/React_frontend/index.html"
+  },
+  {
+    "revision": "892006b0edecc2d6f2af",
+    "url": "/React_frontend/static/css/main.4f5031d5.chunk.css"
+  },
+  {
+    "revision": "d046c724f4f9dee36b14",
+    "url": "/React_frontend/static/js/2.50d71754.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/React_frontend/static/js/2.50d71754.chunk.js.LICENSE"
+  },
+  {
+    "revision": "892006b0edecc2d6f2af",
+    "url": "/React_frontend/static/js/main.77621ccb.chunk.js"
+  },
+  {
+    "revision": "adfaad953c38bbc623a9",
+    "url": "/React_frontend/static/js/runtime-main.086e37de.js"
+  },
+  {
+    "revision": "0f68a05a554534503720f3dd8cfe1626",
+    "url": "/React_frontend/static/media/apple-store.0f68a05a.png"
+  },
+  {
+    "revision": "45e5779c35203d9a367d336e58bbc81a",
+    "url": "/React_frontend/static/media/google-play.45e5779c.png"
+  },
+  {
+    "revision": "3374cc8cf6684e510ee5ca9ed8d2155d",
+    "url": "/React_frontend/static/media/logo1.3374cc8c.svg"
+  }
+]);
